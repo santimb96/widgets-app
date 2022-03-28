b7a506b42640fe1393ac47b6c9d551d3607b65df
Santiago <Martnez Bota>
2022-03-28 11:33:22 +0200

    refactoring trans y lang

--- src/api/translatorApi.js
+++ src/api/translatorApi.js
@@ -1,15 +1,18 @@
-const key = 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM';
+const key = "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM";
 
 const translatorApi = async (code, text) => {
-    const response = await fetch (`https://translation.googleapis.com/language/translate/v2?key=${key}&format=text&target=${code}&q=${encodeURIComponent(text)}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            }
-            
-        })
+  const response = await fetch(
+    `https://translation.googleapis.com/language/translate/v2?key=${key}&format=text&target=${code}&q=${encodeURIComponent(
+      text
+    )}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
-        return await response.json();
-}
-
+  return await response.json();
+};
 export default translatorApi;
--- src/pages/Lang.js
+++ src/pages/Lang.js
@@ -1,33 +1,41 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Dropdown from "../components/langComponents/Dropdown";
 import langOptions from "../services/langOptions";
 import Convert from "../components/langComponents/Convert";
 
 const Lang = () => {
+  const [lang, setLang] = useState(langOptions[0]);
+  const [text, setText] = useState("Hola mundo!");
 
-    const [lang, setLang] = useState(langOptions[0]);
-    const [text, setText] = useState('Hola mundo!');
+  const setLanguage = (term) => {
+    setLang(langOptions?.find((l) => l.label === term));
+  };
 
-    const setLanguage = (term) => {
-        setLang(langOptions?.find(l => l.label === term));
-    }
+  return (
+    <div className="container-fluid">
+      <div className="mt-5 d-flex justify-content-center">
+        <input
+          placeholder="type your text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+      <div className="d-flex justify-content-center">
+        <Dropdown
+          key={lang.value}
+          options={langOptions}
+          selected={lang}
+          selectedLang={setLanguage}
+        />
+      </div>
 
-    return (
-        <div className="container-fluid">
-            <div className="mt-5 d-flex justify-content-center">
-                <input placeholder="type your text" value={text} onChange={(e) => setText(e.target.value)} />
-            </div>
-            <div className="d-flex justify-content-center">
-
-            <Dropdown key={lang.value} options={langOptions} selected={lang} selectedLang={setLanguage}/>
-            </div>
-
-            <div className="d-flex justify-content-center">
-                <div className="mb-5"><Convert lang={lang} text={text}/></div>
-            </div>
-
+      <div className="d-flex justify-content-center">
+        <div className="mb-5">
+          <Convert lang={lang} text={text} />
         </div>
-    )
-} 
+      </div>
+    </div>
+  );
+};
 
 export default Lang;
